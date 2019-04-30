47219f237fddf8c26186b992f99485d38a8d1b42
Onewon <asdzxc13451996@163.com>
2019-04-30 11:29:25 +0800

    v0.1 all temeplate views finished

--- MRS/urls.py
+++ MRS/urls.py
@@ -20,6 +20,7 @@ from django.views.generic.base import TemplateView
 from user import views as v
 from user.views import IndexView
 from moviedata import views as m
+from moviedata.views import detailView,detailbyIDView
 
 urlpatterns = [
     path(r'admin/', admin.site.urls),
@@ -27,8 +28,8 @@ urlpatterns = [
     re_path(r'^search/$', TemplateView.as_view(template_name='search.html'),),
     re_path(r'^search/query$', v.search_detail),
     re_path(r'db$', v.search),
-    re_path(r'^moviedetail/search$',m.detail,{'template_name': 'movie_detail.html'}),
-    re_path(r'^moviedetail/searchbyid$',m.detailbyid,{'template_name': 'movie_detail.html'}),
+    re_path(r'^moviedetail/search$',detailView.as_view(),name=''),
+    re_path(r'^moviedetail/searchbyid$',detailbyIDView.as_view(),name=''),
     path(r'accounts/', include('user.url')),
     path(r'accounts/', include('django.contrib.auth.urls')),
     # url(r'accounts/', include('django.contrib.auth.urls')),
--- moviedata/views.py
+++ moviedata/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 import json,os,requests
+from django.views.generic import TemplateView
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 static_path = BASE_DIR + r"\static\res\movies"
-db_api_t = "http://www.omdbapi.com/?apikey=9be27fce&t={}"
-db_api_i = "http://www.omdbapi.com/?apikey=9be27fce&i={}"
 
 def getcontent(param,useid=False):# 获取api json
+    db_api_t = "http://www.omdbapi.com/?apikey=9be27fce&t={}"
+    db_api_i = "http://www.omdbapi.com/?apikey=9be27fce&i={}"
     content = {}
     if(useid==True):
         db_api_t = db_api_i
@@ -22,11 +23,53 @@ def getcontent(param,useid=False):# 获取api json
         raise e
     else:
         pass
-def detail(request,template_name): #bug
-    param = request.GET.get("title")
-    data = getcontent(param)
-    return render_to_response(template_name,data)
-def detailbyid(request,template_name): #bug
+
+# def detail(request,template_name): #bug
+#     param = request.GET.get("title")
+#     data = getcontent(param)
+#     return render_to_response(template_name,data)
+
+def detailbyid(request,template_name):
     param = request.GET.get("id")
     data = getcontent(param,useid=True)
     return render_to_response(template_name,data)
+    # data : {"id":xxx}
+    # templateView how to get request
+
+class detailView(TemplateView):
+    template_name = 'movie_detail.html'
+    def get_context_data(self, **kwargs):
+        param = self.request.GET.get("title")
+        data = getcontent(param)
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        context['Poster'] = data.get("Poster")
+        context['Title'] = data.get("Title")
+        context['Released'] = data.get("Released")
+        context['Runtime'] = data.get("Runtime")
+        context['Plot'] = data.get("Plot")
+        context['Genre'] = data.get("Genre")
+        context['Actors'] = data.get("Actors")
+        context['Director'] = data.get("Director")
+        context['Language'] = data.get("Language")
+        context['Country'] = data.get("Country")
+        return context
+
+class detailbyIDView(TemplateView):
+    template_name = 'movie_detail.html'
+    def get_context_data(self, **kwargs):
+        param = self.request.GET.get("id")
+        data = getcontent(param,useid=True)
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        context['Poster'] = data.get("Poster")
+        context['Title'] = data.get("Title")
+        context['Released'] = data.get("Released")
+        context['Runtime'] = data.get("Runtime")
+        context['Plot'] = data.get("Plot")
+        context['Genre'] = data.get("Genre")
+        context['Actors'] = data.get("Actors")
+        context['Director'] = data.get("Director")
+        context['Language'] = data.get("Language")
+        context['Country'] = data.get("Country")
+        return context
